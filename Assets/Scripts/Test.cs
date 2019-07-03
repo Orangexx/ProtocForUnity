360bec027519b430e11790a1e1856f7e180bf5af
xiaoxiao <xiaoxiaolala@outlook.com>
2019-07-03 15:50:52 +0800

    初始化

---
+++ Assets/Scripts/Test.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Google.Protobuf;
+using Google.Protobuf.Collections;
+using Google.Protobuf.WellKnownTypes;
+using Example;
+
+public class Test : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Message message = new Message();
+        message.Message_ = "  123123 ";
+
+        byte[] data = message.ToByteArray();
+        Message msg1 = Message.Descriptor.Parser.ParseFrom(data) as Message;
+        var str = JsonFormatter.ToDiagnosticString(msg1);
+        Message msg2 = Message.Descriptor.Parser.ParseJson(str) as Message;
+        Debug.LogFormat("message{0},msg1(byte[] to){1},msg2(jsonStr to){2}",message.Message_,msg1.Message_,msg2.Message_);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
